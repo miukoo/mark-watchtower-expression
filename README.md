7af9dca5c255a3f96e097e2c4d76281af0c8cf21
miukoo <miukoo@126.com>
2019-08-14 00:35:17 +0800

    初始化项目代码

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
 # mark-watchtower-expression
-把前端做成表情.... 把长安十二时辰望楼做成表情..... VUE使用示例，html2canvas+gif.js 学会这个示例，前端每个人都可以做自己的表情图片！！！
+
+> <b style='color:#0a6a;font-size:24px'>长安十二时望楼表情生成程序</b>
+>
+> 最近《长安十二时辰》电视剧火了，其中望楼传递信息在技术圈也掀起了一阵算法浪潮。该项目就是踩着这个热点结合技术可定制化生成个人偏好的望楼表情程序。
+>
+> <span style='color:#ffaa11'>— 喜欢的请点星，或者微信搜索'技术记号'</span>
+
+
+
+### 此程序支持哪些人玩
+
+- 不会前端的你，也可以玩
+- 会前端的，希望你玩出B格
+
+### 支持的功能
+
+- 输入的字符自动转成望楼编码
+
+- 依据输入字符长度，自动调节动画时长
+
+- 生成的动画可直接右键保存为gif图片
+
+- 支持非程序员修改表情文字
+
+  
+
+### 效果演示
+
+![](G:\00_自媒体空间\VUE\mark-watchtower-expression\md\2.gif)
+
+![](G:\00_自媒体空间\VUE\mark-watchtower-expression\md\3.gif)
+
+## A. 你不会程序，可以这样玩
+
+- 第一步：下载a文件夹
+- 第二步：把a文件夹放在一个没有中文路径的地方
+- 第三步：双击a文件夹下的nginx.exe
+- 第四步：在浏览器中访问http://localhost:9898
+
+```javascript
+//  ======================================================
+//  如果你想修改文字，可以修改a/html/index.html文件中的以下几项:
+//  ======================================================
+// 表情的缩放比例
+window['outScale']=1;
+// 表情的红色文字，建议不要超过8个汉字
+window['outTitle']='';
+// 表情的黑色文字，建议不要超过12个汉字
+window['outText']='';
+```
+
+## B. 你会程序，可以这么玩
+
+### 环境依赖
+
+- nodejs
+- webstorm
+- webpack
+- vue
+
+### 安装
+
+``` bash
+# 控制终端输入以下命令，安装相关依赖库
+cnpm i
+```
+
+### 运行
+
+```bash
+# 控制终端输入以下命令，安装相关依赖库
+cnpm run dev
+```
+
+### 高级玩家进阶-源码说明
+
+>程序核心源码在App.vue中，实现的思路是：
+>
+>- 准备1张望楼图片，其中方格处背景透明
+>- 准备12张红色块图片，每一张对应望楼的12个其中一个方格
+>- 把字符转成unicode二进制编码，共计16位
+>- 截取16位的低12位，每一位控制一格望楼方格，如果是1则显示，反正不显示
+
+注：常用汉字3500左右，12位能容纳4096个汉字，赶兴趣的老铁可以自行映射，该项目暂时截断高位字节处理。
+
+#### Model
+
+```java
+	  timer:null,		//定时器
+      text:'',			//计算临时变量
+      showText:'骚等信号接收中...',	//默认显示的等待提示语
+      chars:[],			//outText的字符数组
+      canvas:[],		//截取的gif每贞数据
+      timeStep:500,		//默认滚动时间步长
+      outScale:0.6,		//gif的缩放比例
+      outTitle:'你收到望楼传讯：',		//红色字的内容
+      outText:"语文老师语文不及格",		//黑色字的内容
+      list:[bg_0,bg_1,bg_2,bg_3,bg_4,bg_5,bg_6,bg_7,bg_8,bg_9,bg_10,bg_11],	//红色块图片
+      blag:[true,true,true,true,true,true,true,true,true,true,true,true]//红色块显示控制
+```
+
+#### View
+
+视图代码较为简单，在渲染图片时，通过循环数组输出，注意13张图片需要重叠在一起，并且注意层的顺序。
+
+```html
+<template>
+  <div class="wapper">
+    <div id="gif-body">
+    <div class="bg">
+      <div class="text" style="width: 150px;height: 30px;margin:-150px -80px;left: 100px;">{{outTitle}}</div>
+      <div class="text" style="width: 150px;height: 100px;margin:-50px -20px;left: 50px;color:#000;">{{showText}}</div>
+      <!-- 望楼-楼框样式 -->
+      <img src="./assets/images/bg.png"></div>
+      <!-- 循环输出12位对应的图片,如果隐藏则不输出 -->
+      <template v-for="(item,index) in list">
+        <div v-if="blag[index]" :key="index" :class="className(index)">
+          <img :src="item">
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+```
+
+### VM
+
+VM实现较为复杂，但只要理解一下实现技巧，实现起来依很简单：
+
+- 启动定时器，定时改变页面VIEW的内容（详见：showBlag方法）
+- 每一次修改了VIEW内容之后，创建一个canvas并缓存起来（详见：createCanvas方法）
+- 当字符输出完之后，合并所有的canvas元素，输出成gif动画图片（详见：createGif方法）
---
+++ src/App.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="wapper">
+    <div id="gif-body">
+    <div class="bg">
+      <div class="text" style="width: 150px;height: 30px;margin:-150px -80px;left: 100px;">{{outTitle}}</div>
+      <div class="text" style="width: 150px;height: 100px;margin:-50px -20px;left: 50px;color:#000;">{{showText}}</div>
+      <!-- 望楼-楼框样式 -->
+      <img src="./assets/images/bg.png"></div>
+      <!-- 循环输出12位对应的图片,如果隐藏则不输出 -->
+      <template v-for="(item,index) in list">
+        <div v-if="blag[index]" :key="index" :class="className(index)">
+          <img :src="item">
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+  import GIF from 'gif.js'
+  import html2canvas from 'html2canvas'
+  import bg_0 from './assets/images/bg_0.png'
+  import bg_1 from './assets/images/bg_1.png'
+  import bg_2 from './assets/images/bg_2.png'
+  import bg_3 from './assets/images/bg_3.png'
+  import bg_4 from './assets/images/bg_4.png'
+  import bg_5 from './assets/images/bg_5.png'
+  import bg_6 from './assets/images/bg_6.png'
+  import bg_7 from './assets/images/bg_7.png'
+  import bg_8 from './assets/images/bg_8.png'
+  import bg_9 from './assets/images/bg_9.png'
+  import bg_10 from './assets/images/bg_10.png'
+  import bg_11 from './assets/images/bg_11.png'
+export default {
+  name: 'App',
+  data(){
+    return {
+      timer:null,
+      text:'',
+      showText:'骚等信号接收中...',
+      chars:[],
+      canvas:[],
+      timeStep:500,
+      outScale:0.6,
+      outTitle:'你收到望楼传讯：',
+      outText:"语文老师语文不及格",
+      list:[bg_0,bg_1,bg_2,bg_3,bg_4,bg_5,bg_6,bg_7,bg_8,bg_9,bg_10,bg_11],
+      blag:[true,true,true,true,true,true,true,true,true,true,true,true]
+    }
+  },
+  created(){
+    if(window['outTitle']){
+      this.outTitle=window['outTitle']
+    }
+    if(window['outText']){
+      this.outText=window['outText']
+    }
+    if(window['outScale']){
+      try {
+        this.outScale = parseFloat( window['outScale'])
+      }catch (e) {
+        this.outScale =0.6
+      }
+    }
+  },
+  mounted(){
+    let str = this.outText+" "
+    let temp = str.split('')
+    for(let i=0;i<temp.length;i++){
+      this.chars.push(temp[i])
+    }
+    this.timeStep = 1800/str.length
+    this.start()
+  },
+  methods:{
+    start:function(){
+      let _this = this
+      this.timer = setInterval(function(){_this.showBlag()},this.timeStep)
+    },
+    stop : function(){
+      if(this.timer!=null){
+        clearInterval(this.timer)
+      }
+    },
+    className : function(index){
+      return 'bg_'+index;
+    },
+    getUnicode : function(char){
+      return parseInt(char.charCodeAt(0),10).toString(2);
+    },
+    createGif : function(){
+        var gif = new GIF({
+          workers: 2,
+          quality: 5,
+          workerScript:'/static/gif.worker.js'
+        });
+        for (let i = 0; i <this.canvas.length ; i++) {
+          if(i==this.canvas.length-1){
+            gif.addFrame(this.canvas[i],{delay:2000,copy: true});
+          }else
+            gif.addFrame(this.canvas[i],{delay:this.timeStep,copy: true});
+        }
+        gif.on('finished', function(blob) {
+          window.open(URL.createObjectURL(blob));
+        });
+        gif.render();
+        this.stop()
+    },
+    createCanvas : function() {
+      let _this = this;
+      _this.stop()
+      html2canvas(document.getElementById('gif-body'),{scale:0.6}).then(canvas => {
+        canvas.crossorigin="anonymous"
+        _this.canvas.push(canvas)
+        _this.start()
+      });
+    },
+    showBlag : function() {
+      if(this.showText.indexOf("\.")!=-1){
+        this.showText=this.showText.substring(0,this.showText.length-3)
+      }else{
+        this.showText+="..."
+      }
+      let cha = this.chars.shift()
+      if (cha) {
+        cha = cha.toUpperCase()
+        let uni = this.getUnicode(cha)
+        this.text+=cha
+        let bs = uni.split('');
+        for(let i=0;i<this.blag.length;i++){
+          if(bs[i]=='1'){
+            this.$set(this.blag,i,true)
+          }else{
+            this.$set(this.blag,i,false)
+          }
+        }
+        if(this.chars.length==1){
+          this.showText = this.text
+        }
+        // 创建can
+        this.createCanvas();
+      }
+      else {
+        this.createGif()
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  html,body{height:100%;}
+  *{
+    margin: 0px;
+    padding: 0px;
+    vertical-align: middle;
+    box-sizing: border-box;
+  }
+.wapper {
+  width:100%;
+  height: 100%;
+  overflow: hidden;
+  text-align: center;
+}
+.wapper div{
+    width: 640px;
+    height: 326px;
+    clear: both;
+    position: absolute;
+    top: 50%;
+    left:50%;
+    margin-top: -163px;
+    margin-left: -320px;
+    text-align: left;
+}
+  .text{
+    position:relative;
+    font-size: 36px;
+    color: #ff6666;
+    text-wrap: revert;
+  }
+</style>
